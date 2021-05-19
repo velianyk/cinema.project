21ac428a3f0e6eff613fe7dca46ddba0552ae741
velianyk <sofiykavelianyk15@gmail.com>
2021-05-19 23:34:58 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-/*$(function() {
- let header = $('.header');
-  
- $(window).scroll(function() {
-   if($(this).scrollTop() > 1) {
-    header.addClass('header_fixed');
-   } else {
-    header.removeClass('header_fixed');
-   }
- });
-});*/
 $(function() {
     let header = $('.header');
     let hederHeight = header.height(); 
@@ -36,4 +25,16 @@ function openNav() {
   document.getElementById("nav-modal").style.width = "0";
   }
 
-  
+  var btn1= document.getElementById('btn1');
+  var btn2 = document.getElementById('btn2');
+  var btn3 = document.getElementById('btn3');
+  var btn4 = document.getElementById('btn4');
+
+  btn2.onclick = function()
+  {
+    head_link.addClass('link2');
+  }
+  btn3.onclick = function()
+  {
+    head_link.addClass('link3');
+  }
